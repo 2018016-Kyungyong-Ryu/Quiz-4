0657b0adc047abf27db7ca85ab80355db77e5c79
2018016-Kyungyong-Ryu <144421263+2018016-Kyungyong-Ryu@users.noreply.github.com>
2023-10-06 16:15:13 +0900

    6주차 과제 제출하였습니다.

--- Quiz-4.py
+++ Quiz-4.py
@@ -1,6 +1,6 @@
 def matrix(number):
     print("구구단을 출력합니다. \n")
-    for x in range(2, number+1):
+    for x in range(1, number+1):
         print("------- [" + str(x) + "단] ----------")
         for y in range(1,number+1):
             print(x, "X", y ,"=", x*y)
